8d437ca5d7c24a553ee1e0c7feec672038d4551f
Bia <fabianazs@gmail.com>
2019-09-30 15:19:12 -0300

    Exemplo Sem Singleton

---
+++ Singleton/BolaSemSingleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class BolaSemSingleton
+    {
+        public BolaSemSingleton()
+        {
+            Console.WriteLine("Bola em jogo!");
+        }
+
+
+        public void Mensagem(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+    }
+}
