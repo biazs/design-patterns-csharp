7dfe6e2e6be8b13207e1cac6f065a5bd93efbe54
Bia <fabianazs@gmail.com>
2019-09-30 16:23:34 -0300

    Factory Method

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FactoryMethod fm = new FactoryMethod();
+
+            Console.WriteLine("Liu Kang | SubZero | Scorpion");
+            Console.WriteLine();
+            Console.WriteLine("Escolha seu personagem");
+            string escolha = Console.ReadLine();
+
+            IPersonagem personagem = fm.Escolher_Personagem(escolha);
+            Console.WriteLine();
+
+            Console.WriteLine("Voce vai jogar com ");
+            personagem.Escolhido();
+
+            Console.ReadKey();
+        }
+    }
+}
