2bde993f6593394b8eb71862c7ca51bf0230433a
Bia <fabianazs@gmail.com>
2019-09-30 15:20:17 -0300

    Singleton

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -10,16 +10,16 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
-            BolaSemSingleton jogador_1 = new BolaSemSingleton();
-            //Singleton jogador_1 = Singleton.GetInstancia;
+            //BolaSemSingleton jogador_1 = new BolaSemSingleton();
+            Singleton jogador_1 = Singleton.GetInstancia;
             jogador_1.Mensagem("Jogador 1: A bola esta comigo no meio do campo");
 
-            BolaSemSingleton jogador_2 = new BolaSemSingleton();
-            //Singleton jogador_2 = Singleton.GetInstancia;
+            //BolaSemSingleton jogador_2 = new BolaSemSingleton();
+            Singleton jogador_2 = Singleton.GetInstancia;
             jogador_2.Mensagem("Jogador 2: Recebeu a bola");
 
-            BolaSemSingleton jogador_3 = new BolaSemSingleton();
-            //Singleton jogador_3 = Singleton.GetInstancia;
+            //BolaSemSingleton jogador_3 = new BolaSemSingleton();
+            Singleton jogador_3 = Singleton.GetInstancia;
             jogador_3.Mensagem("Jogador 3: Recebeu o lancamento na linha de fundo");
 
             Console.ReadKey();
