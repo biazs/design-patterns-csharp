42ff95242ede720784e9abb26830ca64fe65f432
Bia <fabianazs@gmail.com>
2019-10-04 16:35:08 -0300

    Padrao Estrutural - Composite

---
+++ Composite/Componente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public abstract class Componente
+    {
+        protected string nome;
+
+        public Componente(string nome)
+        {
+            this.nome = nome;
+        }
+        public abstract void Adicionar(Componente c);
+        public abstract void Remover(Componente c);
+        public abstract void Mostrar(int profundidade);
+    }
+}
---
+++ Composite/Folha.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Composite
+{
+    class Folha : Componente
+    {
+        public Folha(string nome) : base(nome)
+        {
+
+        }
+        public override void Adicionar(Componente c)
+        {
+            Console.WriteLine("Nao e possivel adicionar a folha");
+        }
+
+        public override void Mostrar(int profundidade)
+        {
+            Console.WriteLine(new String('-', profundidade) + nome);
+        }
+
+        public override void Remover(Componente c)
+        {
+            Console.WriteLine("Nao e possivel remover a folha");
+        }
+    }
+}
