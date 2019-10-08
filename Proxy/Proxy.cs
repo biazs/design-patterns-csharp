5b819fd4d6a96989e2b698a3b1186c92bd47a8d3
Bia <fabianazs@gmail.com>
2019-10-08 15:27:31 -0300

    Padrao Estrutural - Proxy

---
+++ Proxy/Proxy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Proxy
+{
+    public class Proxy : Subject
+    {
+        private RealSubject realSubject;
+        public override void Requisicao()
+        {
+            if (this.realSubject == null)
+                this.realSubject = new RealSubject();
+
+            this.realSubject.Requisicao();
+        }
+    }
+}
---
+++ Proxy/RealSubject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proxy
+{
+    class RealSubject : Subject
+    {
+        public override void Requisicao()
+        {
+            Console.WriteLine("Chamando RealSubject.Request()");
+        }
+    }
+}
