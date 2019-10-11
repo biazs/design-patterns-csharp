fee44dffcac0128c6a78fbb2b7b23abe382e4d10
Bia <fabianazs@gmail.com>
2019-10-11 15:09:18 -0300

    Padrao Comportamental - Strategy

---
+++ Strategy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList studentRecords = new SortedList();
+            studentRecords.Add("Maria");
+            studentRecords.Add("Joao");
+            studentRecords.Add("Pedro");
+            studentRecords.Add("Ana");
+            studentRecords.Add("Carlos");
+
+            studentRecords.SetSortStrategy(new QuickSort());
+            studentRecords.Sort();
+            
+
+            studentRecords.SetSortStrategy(new ShellSort());
+            studentRecords.Sort();
+
+
+            studentRecords.SetSortStrategy(new MergeSort());
+            studentRecords.Sort();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Strategy/SortStrategy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Strategy
+{
+    public abstract class SortStrategy
+    {
+        public abstract void Sort(List<string> list);
+    }
+}
