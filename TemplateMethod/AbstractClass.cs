c5be508754e7cdbb9942b396247c633bd4d4d9be
Bia <fabianazs@gmail.com>
2019-10-11 15:17:34 -0300

    Padrao Comportamental - Template Method

---
+++ TemplateMethod/AbstractClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TemplateMethod
+{
+    public abstract class AbstractClass
+    {
+        public abstract void PrimitiveOperation1();
+        public abstract void PrimitiveOperation2();
+
+        public void TemplateMethod()
+        {
+            PrimitiveOperation1();
+            PrimitiveOperation2();
+            Console.WriteLine("");
+        }
+    }
+}
