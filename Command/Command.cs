9fed0f28ed5c7a6959c065302fe797bfd3f49dd5
Bia <fabianazs@gmail.com>
2019-10-11 09:03:40 -0300

    Padrao Comportamental - Command

---
+++ Command/Command.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Command
+{
+    public abstract class Command
+    {
+        protected Receiver receiver;
+
+        protected Command(Receiver rc)
+        {
+            this.receiver = rc;
+        }
+
+        public abstract void Execute();
+    }
+}
---
+++ Command/ConcretCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Command
+{
+    public class ConcretCommand : Command
+    {
+        public ConcretCommand(Receiver receiver) : base(receiver)
+        {
+
+        }
+        public override void Execute()
+        {
+            receiver.Action();
+        }
+    }
+}
